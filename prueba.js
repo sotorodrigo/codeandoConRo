aa1213962b91083fe97febf19258aaf2fcc02c41
sotorodrigo <mdqrodrigosoto@gmail.com>
2021-11-15 16:59:38 -0300

    primer ejercicio

---
+++ prueba.js
@@ -0,0 +1,11 @@
+let interes=2;
+let montoTotal=5000;
+let totalConInteres=0;
+let ganancia=0;
+
+totalConInteres=montoTotal+(2/100)*montoTotal;
+ganancia=(2/100) * montoTotal;
+
+console.log("El usuario gano: "+ganancia+ " y el total de dinero que dispone ahora es de: "+ totalConInteres);
+
+
